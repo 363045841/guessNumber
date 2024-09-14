d30204de5e4595c7373a72792ed4f302787e1f9a
363045841 <161981174@qq.com>
2024-09-14 21:02:29 +0800

    vuetify

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-app>
+    <v-main id="main">
+      <v-app-bar app image="./assets/a4754a82ckef2a11a55447d8f48a81f7.jpg">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <template v-slot:append>
+          <v-btn icon="mdi-refresh"></v-btn>
+          <v-btn icon="mdi-dots-vertical"></v-btn>
+        </template>
+        <v-toolbar-title>Guess Number</v-toolbar-title>
+      </v-app-bar>
+      <v-container class="guess-card">
+        <v-card prepend-icon="mdi-counter" :subtitle="textRealNum" width="600">
+          <template v-slot:title>
+            <span class="font-weight-black">猜数字</span>
+          </template>
+          <v-container>
+            <v-row align="center">
+              <v-col cols="500">
+                <v-text-field label="Enter a number you guess" v-model:model-value="numberChoose" />
+              </v-col>
+              <v-col cols="auto">
+                <v-btn variant="tonal" :ripple="true" @click="checkNumber(numberChoose)">提交</v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-container>
+      <v-container>
+      <v-alert
+        v-if="winAttribute" width="600"
+        text="你答对了"
+        title="Alert title" type="success">
+      </v-alert>
+      <v-alert v-if="showError" width="600"
+      text="你答错了" title="Alert title" type="error">
+      </v-alert>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+let numberChoose = ref<string | null>(null)
+let randomNum = ref(Math.floor(Math.random() * 100))
+let winAttribute = ref<boolean>(false)
+let textRealNum = computed<string>(() => {
+  return '请你猜一个数字,我们会告诉你是大是小,偷偷告诉你数字是' + randomNum.value
+})
+let showError = ref<boolean>(false)
+function checkNumber(inputNum: (string | null)) {
+  numberChoose.value = null
+  console.log(inputNum)
+  if (inputNum === '') {
+    alert('please input a number')
+  }
+  const num = Number(inputNum)
+  if (!Number.isNaN(num)) {
+    if (num > randomNum.value) {
+      alert('too big')
+      showError.value = true
+      setTimeout(() => {
+        showError.value = false
+      }, 2000);
+    } else if (num < randomNum.value) {
+      alert('too small')
+      showError.value = true
+      setTimeout(() => {
+        showError.value = false
+      }, 2000);
+    } else {
+      winAttribute.value = true
+      setTimeout(() => {
+        winAttribute.value = false
+        randomNum.value = Math.floor(Math.random() * 100)
+      }, 2000);
+    }
+  } else {
+    alert('please input a number')
+  }
+}
+</script>
+
+<style></style>
